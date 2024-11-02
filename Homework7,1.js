42aaf1efbc520c38c6a8572df139f9f6b0c9db53
Zhenya-228 <shilo.evgeny@gmail.com>
2024-11-03 00:10:27 +0200

    add homework

--- Homework7,1.js
+++
@@ -1,7 +0,0 @@
-const originalArray = [1, 2, 3, 4, 5];
-
-function reversedArray (arr){
-    return arr.slice().reverse();
-}
-
-console.log(reversedArray(originalArray)); // [5, 4, 3, 2, 1]
--- Homework7,2.js
+++
@@ -1,7 +0,0 @@
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-function uniqueValues(arr1, arr2) {
-  
-  return arr1.concat(arr2).filter((value, index, array) => array.indexOf(value) === index);;
-}
-console.log(uniqueValues(array1, array2)); // [1, 2, 3, 4, 5, 6, 7]
